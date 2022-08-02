3685b725903e51bd82ba8a9748066d3470bed6ab
Felipe <fellipefreiire3@gmail.com>
2022-08-02 17:58:09 -0300

    config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,8 +1,27 @@
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
+
+const domain = process.env.PRODUCTION_DOMAIN
 
 module.exports = {
   mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/about/latest/',
+  },
   devtool: 'source-map',
-  plugins: [new BundleAnalyzerPlugin()],
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'about',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AboutApp': './src/bootstrap',
+        './Section': './src/components/Section/index.tsx',
+      },
+      remotes: {
+        home: `home@${domain}/home/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
 }
