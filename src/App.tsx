af04b67fbdc4d95c1d9400dc7478cdcdf701181d
Felipe <fellipefreiire3@gmail.com>
2022-08-03 23:21:37 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './styles/global/styles.css'
-import { Sidebar } from 'home/Sidebar'
 import { Section } from './components/Section'
 
 export const App = () => {
   return (
     <div>
-      <Sidebar />
       <Section />
     </div>
   )
